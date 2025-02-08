912b8a847e6c1d09702787c01c925ca23ce71216
Angerbode <jdr@frichs-pyrolysis.com>
2025-02-08 17:50:06 +0100

    split words, count words

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def main():
     path = "books/frankenstein.txt"
+    #open file
+    word_Count = 0
     with open(path) as f:
         file_contents = f.read()
-    print(file_contents)
+
+    #split into individual words
+    words = file_contents.split()
+    
+    #count the words
+    for word in words:
+        word_Count += 1
+
+    print(word_Count)
 
 main()
