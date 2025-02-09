c81d40eb46cf628e01bac689c4ba812dbcef2fb2
Angerbode <jdr@frichs-pyrolysis.com>
2025-02-09 13:51:15 +0100

    added individual character count

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 def main():
+    character_Count = {}         
     path = "books/frankenstein.txt"
     #open file
     word_Count = 0
     with open(path) as f:
         file_contents = f.read()
-
+    #remove capital letters
+    lower_string = file_contents.lower()
     #split into individual words
     words = file_contents.split()
-    
+
     #count the words
     for word in words:
         word_Count += 1
 
-    print(word_Count)
+    #count individual characters
+    characters =list(lower_string)
+    for character in characters:
+        if character not in character_Count:
+            character_Count[character] = 1
+        else:    
+            character_Count[character] += 1 
 
+    #sort into list of dictionaries, convert to list of dict
+    char_list = [] 
+    for char, count in character_Count.items():
+        if char.isalpha():
+            char_list.append({"char": char, "num": count})
+    
+    char_list.sort(reverse=True, key=sort_on)
+    print(f"there are {word_Count} words in the book")
+    print(f"heres a list of individual character counts in the book: \n {char_list}")
+
+#sorting hat
+def sort_on(dict):
+    return dict["num"]
 main()
